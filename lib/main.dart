2aac47f985b420f7b8d58917500b5afb4003ce07
Trenton von Sosen <Trenton@von-Sosen-MacBook-Pro.local>
2020-11-08 12:55:46 -0800

    My second commit

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
+      // ! change to MyHomePage for already setup screen
     );
   }
 }
--- lib/screens/homepagenotsetup.dart
+++ lib/screens/homepagenotsetup.dart
@@ -30,38 +30,27 @@ homePageNotSet(BuildContext context) {
             ),
             Container(
               margin: EdgeInsets.only(top: 60),
-              child: garageStatus(3),
-        //       child: Row(
-        //         mainAxisAlignment: MainAxisAlignment.center,
-        //         children:[
-        //         Container(
-        //           width: 20,
-        //           height: 20,
-        //           margin: EdgeInsets.all(10.0),
-        //           decoration: BoxDecoration(
-        //             color: Colors.orange,
-        //             shape: BoxShape.circle
-        //   ),
-        // ),
-        //           Text("Current Status: Unknown", style: currentStatusStyle),
-                // ],
-                // ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children:[
+                Container(
+                  width: 20,
+                  height: 20,
+                  margin: EdgeInsets.all(10.0),
+                  decoration: BoxDecoration(
+                    color: Colors.orange,
+                    shape: BoxShape.circle
+          ),
+        ),
+                  Text("Garage Not Setup", style: currentStatusStyle),
+                ],
+                ),
                
             ), 
                 
            
           
-            Container(
-                height: 300,
-                width: 400,
-                margin: EdgeInsets.only(top: 30, left: 15, right: 15),
-                decoration: BoxDecoration(
-                    border: Border.all(color: Colors.black87),
-                    borderRadius: BorderRadius.all(Radius.circular(30))),
-                child: Column(children: [
-                  Text('Other Garages', style: addButton),
-                ])
-                ),
+          
             Container(
               margin: EdgeInsets.only(top:50),
               width: 100,
@@ -77,7 +66,17 @@ homePageNotSet(BuildContext context) {
                   ),
                     
               ), 
-            
+              Container(
+                height: 300,
+                width: 400,
+                margin: EdgeInsets.only(top: 50, left: 15, right: 15),
+                decoration: BoxDecoration(
+                    border: Border.all(color: Colors.black87),
+                    borderRadius: BorderRadius.all(Radius.circular(30))),
+                child: Column(children: [
+                  Text('Other Garages', style: addButton),
+                ])
+                ),
               
             
           ],
